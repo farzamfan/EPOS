ce503ca2bd148edf2aecc71cab9bdda7c7bcb0e6
Farzam <farzam.fanni@gmail.com>
2019-07-02 16:18:10 +0200

    Init
    FlexibleIEPOS

--- src/agent/planselection/PlanSelectionOptimizationFunction.java
+++
@@ -1,9 +0,0 @@
-package agent.planselection;
-
-import java.util.HashMap;
-
-public interface PlanSelectionOptimizationFunction {
-	
-	public double apply(HashMap<OptimizationFactor, Object> parameters);
-
-}
